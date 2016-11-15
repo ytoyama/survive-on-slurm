5774ef66f211f17653ab547f5a325a7f8dccb508
Yota Toyama <raviqqe@gmail.com>
2016-11-16 06:19:19 +0900

    Install tensorflow with pip

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -4,18 +4,20 @@
 
 scratch_dir=/scratch/ytoyama
 bazel_dir=$scratch_dir/bazel
+anaconda_dir=$HOME/anaconda3
+anaconda_bin_dir=$anaconda_dir/bin
 
 export CFLAGS="-march=k8 -mtune=generic"
 export CXXFLAGS=$CFLAGS
-export CC=$HOME/anaconda3/bin/gcc
-export LIBRARY_PATH=$HOME/anaconda3/lib64
+export CC=$anaconda_bin_dir/gcc
+export LIBRARY_PATH=$anaconda_dir/lib64
 export LD_LIBRARY_PATH=$LIBRARY_PATH
 
 for bin in as ld nm
 do
-  if [ ! -f $HOME/anaconda3/bin/$bin ]
+  if [ ! -f $anaconda_bin_dir/$bin ]
   then
-    ln -s /usr/bin/$bin $HOME/anaconda3/bin/$bin
+    ln -s /usr/bin/$bin $anaconda_bin_dir/$bin
   fi
 done
 
@@ -34,9 +36,11 @@ then
   git clone https://github.com/tensorflow/tensorflow
 fi &&
 
+pkg_dir=$HOME/tensorflow_pkg
+
 cd tensorflow &&
 git checkout r0.11 &&
 ./configure < $HOME/bin/tf_config.txt &&
 bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package &&
-bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg &&
-ls /tmp/tensorflow_pkg
+bazel-bin/tensorflow/tools/pip_package/build_pip_package $pkg_dir &&
+pip install $pkg_dir/tensorflow-0.11.0-py3-none-any.whl
